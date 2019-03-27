a13446f205b8ff01269fc4922d18caa39bc879d2
Dale Auten <dauten96@gmail.com>
2019-03-26 22:21:30 -0500

    skeleton of min-heuristics

--- nqueens.cpp
+++ nqueens.cpp
@@ -5,6 +5,7 @@
 #include <string>
 
 static int count = 0;
+
 int absVal(int i){
   if(i < 0)
     return (i*-1);
@@ -18,19 +19,41 @@ int legal(int x2, int y2, int *result) {
 
 		if ((result[i] == y2)	|| (absVal(i - x2) == absVal(result[i] - y2))) {
 			return 0;
+      result[i] = -1;
 		}
 	}
 	return 1;
 }
 
+int legalBoard(int * result, int length){
+  int valid = 1;
+  for(int x = 0; x < length; x++){
+
+    if( !legal(x, result[x], result) ){
+      return 0;
+    }
+  }
+  return 1;
+}
+
+//given blank board, fill it with queens randomly.
+//conflicts matrix is filled with number of queens that
+//threaten that particular space
+int generateBoard(int * result, int ** conflicts){
+
+}
+
+
+
+
 void printSolution(int * result, int size){
   for(int i = 0; i < size; i++){
     for(int y = 0; y < result[i]; y++){
-      printf(".");
+      printf(". ");
     }
-    printf("%d", result[i]);
+    printf("%d ", result[i]);
     for(int y = result[i]+1; y < size; y++){
-      printf(".");
+      printf(". ");
     }
     printf("\n");
   }
@@ -39,22 +62,25 @@ void printSolution(int * result, int size){
 
 void placeQueens(int x, int size, int * result, int first) {
 	for (int i = 0; i < size; i++) {
-		//check if queen at xth row can be placed at i-th column.
+
 		if (legal(x, i, result)) {
-			result[x] = i; // place the queen at this position.
+			result[x] = i;
 			if (x == size - 1) {
           printf("Solution found:\n");
           printSolution(result, size);
           count++;
-          if(count > 2)
+          printf("legal?%d\n", legalBoard(result, size));
+          if(count > 2){
+
             exit(0);
+          }
 			}
 			placeQueens(x + 1, size, result, first);
 		}
 	}
 }
-/*
 
+/*
 procedure AC-1
 Q ← {(V i , V j ) ∈ arcs(G), i ≠ j};
 repeat
@@ -64,8 +90,8 @@ repeat
   endfor;
 until not(CHANGE);
 end_AC
-
 */
+
 int main() {
   int n, q;
   printf("Insert n size:\n");
@@ -74,5 +100,7 @@ int main() {
   std::cin >> q;
 	int result[n];
   result[0] = q;
+  printf("%d\n",legalBoard(result, n));
   placeQueens(1, n, result, 0);
+
 }
