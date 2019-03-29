c71df338abaccf041b5f507f928c25393a4bd24f
Dale Auten <dauten96@gmail.com>
2019-03-28 22:44:31 -0500

    some progress on min-conflicts alg

--- nqueens.cpp
+++ nqueens.cpp
@@ -3,8 +3,23 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <time.h>
+
 
 static int count = 0;
+
+void printSolution(int result[], int size){
+  for(int i = 0; i < size; i++){
+    for(int y = 0; y < result[i]; y++){
+      printf(". ");
+    }
+    printf("%d ", result[i]);
+    for(int y = result[i]+1; y < size; y++){
+      printf(". ");
+    }
+    printf("\n");
+  }
+}
 
 int absVal(int i){
   if(i < 0)
@@ -14,7 +29,7 @@ int absVal(int i){
     return i;
 }
 
-int legal(int x2, int y2, int *result) {
+int legal(int x2, int y2, int result[]) {
 	for (int i = 0; i < x2; i++) {
 
 		if ((result[i] == y2)	|| (absVal(i - x2) == absVal(result[i] - y2))) {
@@ -25,7 +40,24 @@ int legal(int x2, int y2, int *result) {
 	return 1;
 }
 
-int legalBoard(int * result, int length){
+int conflict(int x1, int y1, int result[], int size){
+  int c, x, y = 0;
+  c=0;
+//  printf("c is now %d\n", c);
+  for(x = 0; x < size; x++){
+    if(x != x1 || y1 != result[x]){
+      //if this is a different space (we don't want to cound collisions wit ourself)
+      if(x == x1 || y1 == result[x] || absVal(x-x1)==absVal(result[x]-y1)){
+        c++;
+  //      printf("c is now %d\n", c);
+      }
+    }
+  }
+  printf("c is %d\n", c);
+  return c;
+}
+
+int legalBoard(int result[], int length){
   int valid = 1;
   for(int x = 0; x < length; x++){
 
@@ -39,68 +71,67 @@ int legalBoard(int * result, int length){
 //given blank board, fill it with queens randomly.
 //conflicts matrix is filled with number of queens that
 //threaten that particular space
-int generateBoard(int * result, int ** conflicts){
+int generateBoard(int result[], int size){
+  int i;
+  for(i = 0; i < size; i++ ){
+    result[i] = i;
+  }
 
-}
+  int a, b;
+  for(i = 0; i < size; i++){
+    a = rand();
+    a = a % size;
+    b = rand();
+    b = b % size;
 
-
-
-
-void printSolution(int * result, int size){
-  for(int i = 0; i < size; i++){
-    for(int y = 0; y < result[i]; y++){
-      printf(". ");
-    }
-    printf("%d ", result[i]);
-    for(int y = result[i]+1; y < size; y++){
-      printf(". ");
-    }
-    printf("\n");
+    int temp = result[a];
+    result[a] = result[b];
+    result[b] = temp;
   }
 }
 
 
-void placeQueens(int x, int size, int * result, int first) {
-	for (int i = 0; i < size; i++) {
-
-		if (legal(x, i, result)) {
-			result[x] = i;
-			if (x == size - 1) {
-          printf("Solution found:\n");
-          printSolution(result, size);
-          count++;
-          printf("legal?%d\n", legalBoard(result, size));
-          if(count > 2){
-
-            exit(0);
-          }
-			}
-			placeQueens(x + 1, size, result, first);
-		}
-	}
+//returns the column of result[row] with the fewest conflicts
+int safestCol(int result[], int size, int row){
+  int min = 1000000;
+  int best = result[row];
+  for(int col = 0; col < size; col++){
+    int t = conflict(row, col, result, size);
+    if( t < min ){
+      min = t;
+      best = col;
+    }
+  }
+  return best;
 }
 
-/*
-procedure AC-1
-Q ← {(V i , V j ) ∈ arcs(G), i ≠ j};
-repeat
-  CHANGE ← false;
-  for each (V i , V j ) ∈ Q do
-    CHANGE ← (REVISE(V i , V j ) or CHANGE);
-  endfor;
-until not(CHANGE);
-end_AC
-*/
 
 int main() {
-  int n, q;
+  int n, q, x, y;
+  srand(time(NULL));   // Initialization, should only be called once.
   printf("Insert n size:\n");
   std::cin >> n;
   printf("Insert first Queen Location:\n");
-  std::cin >> q;
-	int result[n];
-  result[0] = q;
-  printf("%d\n",legalBoard(result, n));
-  placeQueens(1, n, result, 0);
+  //std::cin >> q;
+  int result[n];
+//  result[n];
+  generateBoard(result, n);
+
+  int a;
+  int patience = 10000;
+  while(!legalBoard(result, n) && patience--){
+    a = rand() % n;
+    printf("safest column of %dth row would be %d\n", a, safestCol(result, n, a));
+    result[a] = safestCol(result, n, a);
+    printSolution(result, n);
+
+  }
+
+  if(legalBoard(result, n)){
+    printf("We did it!\n");
+  }
+  else{
+    printf("I give up!\n");
+  }
 
 }
